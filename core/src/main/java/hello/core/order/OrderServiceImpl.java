8e461a98e3f5b373a2929f3b7f1251aadafc1cce
jiseong0612 <hth5545@gmail.com>
2022-02-22 13:02:23 +0900

    test 정률할인 10퍼센트 테스트 vip or basic

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,14 @@ package hello.core.order;
 
 import hello.core.discount.Discountpolicy;
 import hello.core.discount.FixDiscountPolicy;
-import hello.core.member.Grade;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 	private final MemberRepository memberRepository = new MemoryMemberRepository();
-	private final Discountpolicy discountPolicy = new FixDiscountPolicy();
+	private final Discountpolicy discountPolicy = new RateDiscountPolicy();
 
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
