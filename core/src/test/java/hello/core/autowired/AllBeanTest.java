b83afc2a1a270ee0cb4654bdd14a0e8379963c29
jiseong0612 <hth5545@gmail.com>
2022-02-26 11:13:39 +0900

    test 스프링 빈 생명주기 (prototype, singleton)

--- core/src/test/java/hello/core/autowired/AllBeanTest.java
+++ core/src/test/java/hello/core/autowired/AllBeanTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
 
 import hello.core.AutoAppConfig;
 import hello.core.discount.Discountpolicy;
@@ -20,8 +21,7 @@ public class AllBeanTest {
 		ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class ,DiscountService.class);
 		DiscountService discountService =  ac.getBean(DiscountService.class);
 		Member member = new Member(1L, "userA", Grade.VIP);
-		int discountPrice = discountService.discount(member, 50000, "fixDiscount");
-		Assertions.assertThat(discountService).isInstanceOf(DiscountService.class);
+		int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
 		Assertions.assertThat(discountPrice).isEqualTo(1000);
 	}
 	
@@ -29,7 +29,6 @@ public class AllBeanTest {
 		private final Map<String, Discountpolicy> policyMap;
 		private final List<Discountpolicy> policies;
 		
-		@Autowired
 		public DiscountService(Map<String, Discountpolicy> policyMap, List<Discountpolicy> policies) {
 			this.policyMap = policyMap;
 			this.policies = policies;
