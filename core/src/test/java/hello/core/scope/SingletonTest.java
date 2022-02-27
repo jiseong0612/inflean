865493b052f90cf089112b668717f27d4232c4e1
jiseong0612 <hth5545@gmail.com>
2022-02-27 23:32:18 +0900

    test 싱글톤 생성 후 특정 로직 실행시 프로토타입 빈 생성

--- core/src/test/java/hello/core/scope/SingletonTest.java
+++ core/src/test/java/hello/core/scope/SingletonTest.java
@@ -5,8 +5,12 @@ import javax.annotation.PreDestroy;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+
+import hello.core.scope.PrototypeTest.PrototypeBean;
 
 public class SingletonTest {
 
@@ -23,9 +27,18 @@ public class SingletonTest {
 	
 	@Scope("singleton")
 	static class SingletonBean{
+		
+		@Autowired
+		ApplicationContext ac;
+		
 		 @PostConstruct
 		 public void inint() {
 			 System.out.println("싱글톤 초기화......");
+		 }
+		 
+		 //싱글톤에서 호출할 떄 마다 프로토타입 빈을 생성한다..
+		 public void logic() {
+			 PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
 		 }
 		 
 		 @PreDestroy
